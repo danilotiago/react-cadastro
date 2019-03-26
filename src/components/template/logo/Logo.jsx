801789c64b8980d256ebecca9668d63aea92cf65
danilotiago <programacao.desenvolvimento@gmail.com>
2019-03-26 15:08:24 -0300

    ajusatando css componente logo

--- src/components/template/logo/Logo.jsx
+++ src/components/template/logo/Logo.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
 
 import './Logo.css'
+import logo from '../../../assets/images/react.png'
 
 export default props =>
     <aside className="logo">
-        Logo
+        <a href="/" className="logo">
+            <img src={logo} alt="Logo React"/>
+        </a>
     </aside>
