6485d0d0e343fc6134d348031d0c89ce454e659a
danilotiago <programacao.desenvolvimento@gmail.com>
2019-03-26 15:42:53 -0300

    ajusatando css componente de navegacao

--- src/components/template/nav/Nav.jsx
+++ src/components/template/nav/Nav.jsx
@@ -4,5 +4,13 @@ import './Nav.css'
 
 export default props =>
     <aside className="menu-area">
-        Navegação
+        <nav className="menu">
+            {/* criar componente para os links de menu NavItem */}
+            <a href="#/">
+                <i className="fa fa-home"></i> Início
+            </a>
+            <a href="#/">
+                <i className="fa fa-users"></i> Usuários
+            </a>
+        </nav>
     </aside>
