8c5252d3f5d0984e641636f7e093e00cbdc1fd5e
danilotiago <programacao.desenvolvimento@gmail.com>
2019-03-27 10:17:35 -0300

    criando formulario de cadastro de usuarios

--- src/components/user/UserComponent.jsx
+++ src/components/user/UserComponent.jsx
@@ -47,10 +47,74 @@ export default class UserComponent extends Component {
         return list
     }
 
+    // atualiza um unico campo
+    updateField(event) {
+        // como vamos modificar um usuario, faremos o clone deste
+        const user = {...this.state.user}
+        // pega o nome do atributo de acordo com o nome do input
+        // pega o valor do input e atribui ao atributo que tem o nome do input
+        user[event.target.name] = event.target.value
+        // atualiza o estado
+        this.setState({user})
+    }
+
+    // funcao que retorna o JSX do formulario
+    renderForm() {
+        return (
+            <div className="form">
+                <div className="row">
+                    <div className="col-12 col-md-6">
+                        <div className="form-group">
+                            <label>Nome</label>
+                            {/* neste input vamos usar a solucao da arrow ao inves do bind do this */}
+                            <input
+                                type="text"
+                                className="form-control"
+                                name="name"
+                                value={this.state.user.name}
+                                onChange={e => this.updateField(e)}
+                                placeholder="Digite o nome..."
+                            />
+                        </div>
+                    </div>
+
+                    <div className="col-12 col-md-6">
+                        <div className="form-group">
+                            <label>E-mail</label>
+                            {/* neste input vamos usar a solucao da arrow ao inves do bind do this */}
+                            <input
+                                type="text"
+                                className="form-control"
+                                name="email"
+                                value={this.state.user.email}
+                                onChange={e => this.updateField(e)}
+                                placeholder="Digite o e-mail..."
+                            />
+                        </div>
+                    </div>
+                </div>
+
+                <hr />
+                <div className="row">
+                    <div className="col-12 d-flex justify-content-end">
+                        <button
+                            className="btn btn-primary"
+                            onClick={e => this.save(e)}
+                        >Salvar</button>
+                        <button
+                            className="btn btn-secondary ml-2"
+                            onClick={e => this.clear(e)}
+                        >Cancelar</button>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+
     render() {
         return (
             <Main {...headerProps}>
-                Cadastro de usuário
+                {this.renderForm()}
             </Main>
         )
     }
