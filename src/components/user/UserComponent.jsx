7dce6b6571f76a093eb1f1d5f993ca07357c56b8
danilotiago <programacao.desenvolvimento@gmail.com>
2019-03-27 09:56:52 -0300

    criando logica de adicao e alteracao de usuarios

--- src/components/user/UserComponent.jsx
+++ src/components/user/UserComponent.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import axios from 'axios'
 
 import Main from '../template/main/Main'
 
@@ -8,8 +9,43 @@ const headerProps = {
     subtitle: 'Gerenciamento de usuários'
 }
 
+{/* refatorar para um arquivo de constantes e object freeze */}
+const baseUrl = 'http://localhost:3001/users'
+const initialState = {
+    user: {name: '', email: ''},
+    list: []
+}
+
 export default class UserComponent extends Component {
-    state = {...headerProps}
+    state = {...initialState}
+
+    clear() {
+        this.setState({
+            user: initialState.user
+        })
+    }
+
+    save() {
+        const user = this.state.user
+        // estrategia para tratar atualizacao e criacao
+        const method = user.id ? 'put' : 'post'
+        // estrategia para tratar a URL final caso atualizacao ou criacao
+        const url = user.id ? `${baseUrl}/${user.id}` : baseUrl
+
+        axios[method](url, user)
+            .then(resp => {
+                const list = this.getUpdatedList(resp.data)
+                this.setState({user: initialState.user, list})
+            })
+    }
+
+    getUpdatedList(user) {
+        // removemos o user recebido da lista pelo ID
+        const list = this.state.list.filter(u => u.id !== user.id)
+        // adiciona o usuario atualizado na primeira posicao
+        list.unshift(user)
+        return list
+    }
 
     render() {
         return (
